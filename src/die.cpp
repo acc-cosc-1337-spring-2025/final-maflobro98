6970173077651ef5f884f835e191618b6f7eb528
maflobro98 <marco.flores@g.austincc.edu>
2025-05-18 00:44:49 +0000

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,10 @@
-//
+#include <iostream>
+#include <cstdlib>
+#include "die.h"
+
+using namespace std;
+
+int roll()
+{
+    return rand() % 7;
+}
--- src/die.h
+++ src/die.h
@@ -1,1 +1,19 @@
-//
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+#ifndef DIE_H
+#define DIE_H
+
+using namespace std;
+
+class Die
+{
+    public:
+        int roll();
+
+    private:
+        int sides = 6;
+};
+
+#endif
